ac617f06270602ba7dcda1751308a0b49dcc6cdd
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-16 20:25:11 +0200

    smaller jpgs

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ function App() {
 					scissors tool [i], cut out object, select > toggle quick mask [shift-q], paintbrush tool [p], black = subtract, white = add, space = pan around, click shift for straight line, when finished then [shift-q], [enter], color > hue-saturation > lightness
 			</div>
 				<div className="imagesArea">
-					<img src="images/masks1.png" alt="img" />
-					<img src="images/masks2.png" alt="img" />
+					<img src="images/masks1.jpg" alt="img" />
+					<img src="images/masks2.jpg" alt="img" />
 				</div>
 			</div>
 			<div className="item">
@@ -20,8 +20,8 @@ function App() {
 					foreground select tool, roughly select object, close loop, press [enter], blue overlay, change color if you need to, increase radius, brush on foreground, click "preview mask", select "draw background", brush on unwanted background, click "select"
 				</div>
 				<div className="imagesArea">
-					<img src="images/foreground1.png" alt="img" />
-					<img src="images/foreground2.png" alt="img" />
+					<img src="images/foreground1.jpg" alt="img" />
+					<img src="images/foreground2.jpg" alt="img" />
 				</div>
 			</div>
 		</div>
