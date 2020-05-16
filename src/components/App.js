83f5dbde32ac30d0bb99545f71f0993c011eb6e2
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-16 20:08:47 +0200

    second item

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,20 @@ function App() {
 				<h1>Change lighting on one object</h1>
 				<div className="info">
 					scissors tool [i], cut out object, select > toggle quick mask [shift-q], paintbrush tool [p], black = subtract, white = add, space = pan around, click shift for straight line, when finished then [shift-q], [enter], color > hue-saturation > lightness
-
+			</div>
+				<div className="imagesArea">
+					<img src="images/masks1.png" alt="img" />
+					<img src="images/masks2.png" alt="img" />
+				</div>
+			</div>
+			<div className="item">
+				<h1>Convert background of image to transparent</h1>
+				<div className="info">
+					foreground select tool, roughly select object, close loop, press [enter], blue overlay, change color if you need to, increase radius, brush on foreground, click "preview mask", select "draw background", brush on unwanted background, click "select"
 				</div>
 				<div className="imagesArea">
-					<img src="images/masks1.png" alt="masks1" />
-					<img src="images/masks2.png" alt="masks2" />
+					<img src="images/foreground1.png" alt="img" />
+					<img src="images/foreground2.png" alt="img" />
 				</div>
 			</div>
 		</div>
