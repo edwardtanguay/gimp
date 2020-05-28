5637d32555669e68c632aa85d2a2218a6adcdf6a
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-28 22:03:01 +0200

    layerBlending

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,16 @@ function App() {
 	return (
 		<div>
 			<div className="item">
+				<h1>Layer Blending</h1>
+				<div className="info">
+					put main photo on a lower layer than overlay graphic, switch to legacy blending, change mode to grain merge, use opacity slider for fine tuning
+			</div>
+				<div className="imagesArea">
+					<img src="images/layerBlending1.jpg" alt="img" />
+					<img src="images/layerBlending2.jpg" alt="img" />
+				</div>
+			</div>
+			<div className="item">
 				<h1>Change lighting on one object</h1>
 				<div className="info">
 					scissors tool [i], cut out object, select > toggle quick mask [shift-q], paintbrush tool [p], black = subtract, white = add, space = pan around, click shift for straight line, when finished then [shift-q], [enter], color > hue-saturation > lightness
